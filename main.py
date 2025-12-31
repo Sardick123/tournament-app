d5d814e8bcc976697f31775481e9ba21b5ef7683
Sardick123 <sardicklarweh@gmail.com>
2025-12-31 08:43:29 +0000

    Fix logging statement formatting in main.py

--- main.py
+++ main.py
@@ -553,8 +553,7 @@ def update_tournament_status(
             )
         conn.commit()
         logger.info(
-            f"T_ID {tournament_id} status updated to {new_status}. Winner: {
-                winner_username if winner_username else 'N/A'}"
+            f"T_ID {tournament_id} status updated to {new_status}. Winner: {winner_username if winner_username else 'N/A'}"
         )
         return cursor.rowcount > 0
     except sqlite3.Error as e:
